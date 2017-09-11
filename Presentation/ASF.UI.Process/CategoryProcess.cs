d19e920ceb81c6a4e013b3b58fb7afb64fe1047d
arigos <gonzaloarigos@gmail.com>
2017-09-11 19:50:20 -0300

    Add project files.

---
+++ Presentation/ASF.UI.Process/CategoryProcess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using ASF.Entities;
+using ASF.Services.Contracts;
+using ASF.UI.Process;
+
+namespace ASF.UI.Process
+{
+    public class CategoryProcess : ProcessComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Category> SelectList()
+        {
+            var response = HttpGet<AllResponse>("rest/Category/All", new Dictionary<string, object>(), MediaType.Json);
+            return response.Result;
+        }
+
+        public Category FindById(int id)
+        {
+            var parameters = new Dictionary<string, object>();
+            parameters.Add("id",id);            
+
+            var response = HttpGet<FindResponse>("rest/Category/Find", parameters, MediaType.Json);
+            return response.Result;
+        }
+
+        public void Create(Category cat)
+        {
+            HttpPost<Category>("rest/Category/Add", cat, MediaType.Json);
+        }
+
+        public void FindById(Category objeto)
+        {         
+            HttpPost<Category>("rest/Category/Edit", objeto, MediaType.Json);
+        }
+
+        public void Remove(int id)
+        {
+            var parameters = new Dictionary<string, object>();
+            parameters.Add("id", id);
+
+           HttpGet<FindResponse>("rest/Category/Remove", parameters, MediaType.Json);
+        }
+    }
+}
---
+++ Presentation/ASF.UI.WbSite/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using ASF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            Process.CategoryProcess process = new Process.CategoryProcess();
+            var lista = process.SelectList();
+            return View(lista);
+        }
+
+        public ActionResult Find(int id)
+        {
+            Process.CategoryProcess process = new Process.CategoryProcess();
+            var lista = process.FindById(id);
+            return View(lista);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category cat)
+        {
+            Process.CategoryProcess process = new Process.CategoryProcess();
+            process.Create(cat);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult FindByEdit(int id)
+        {
+            Process.CategoryProcess process = new Process.CategoryProcess();
+            var lista = process.FindById(id);
+            return View(lista);
+        }
+
+        [HttpPost]
+        public ActionResult FindByEdit(Category objeto)
+        {
+            Process.CategoryProcess process = new Process.CategoryProcess();
+            process.FindById(objeto);
+           return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Remove(int id)
+        {
+            Process.CategoryProcess process = new Process.CategoryProcess();
+            process.Remove(id);
+           return RedirectToAction("Index");
+        }
+
+    }
+}
